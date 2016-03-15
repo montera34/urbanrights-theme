4c22622ca08c2085fa20fb1400e122964bf32128
skotperez <voraginebramante@gmail.com>
2016-03-15 15:44:33 +0100

    Featured video in Declarations archive template

--- functions.php
+++ functions.php
@@ -133,9 +133,10 @@ function urbanrights_scripts() {
 	wp_enqueue_style( 'urbanrights-style', get_stylesheet_uri(),array('bootstrap-style') );
 
 	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri(). '/bootstrap/js/bootstrap.min.js',array('jquery'),true );
-	wp_enqueue_script( 'urbanrights-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'urbanrights-js', get_template_directory_uri() . '/js/urbanrights.js', array('jquery'), true );
+//	wp_enqueue_script( 'urbanrights-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'urbanrights-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+//	wp_enqueue_script( 'urbanrights-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -163,6 +164,17 @@ function urbanrights_extra_scripts_styles() {
 }
 /* Load scripts for IE compatibility */
 add_action('wp_head','urbanrights_extra_scripts_styles',999);
+
+// custom loops for each template
+function urbanrights_custom_args_for_loops( $query ) {
+	if ( $query->query_vars['post_type'] == 'declarations' && $query->is_main_query() ) { 
+		$query->set( 'nopaging','true');
+		$query->set( 'order','ASC');
+		$query->set( 'orderby','menu_order');
+	}
+	return $query;
+} // END custom args for loops
+add_filter( 'pre_get_posts', 'urbanrights_custom_args_for_loops' );
 
 /**
  * Implement the Custom Header feature.
--- template-parts/content-declarations.php
+++ template-parts/content-declarations.php
@@ -9,13 +9,18 @@
 global $count;
 $count++;
 $video_url = get_post_meta($post->ID,'declaration-video-url',true);
-$declaration_classes = array('col-md-3');
+if ( $post->menu_order == '0' ) {
+	$count = 0;
+	$declaration_classes = array('col-md-12 declaration-featured');
+} else {
+	$declaration_classes = array('col-md-3');
+}
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class($declaration_classes); ?>>
 	<?php echo apply_filters('the_content',$video_url); ?>
 </article><!-- #post-## -->
-<?php if ( $count == 4 ) {
+<?php if ( $count == 4 || $count == 0 ) {
 	echo "<div class='clearfix visible-md-block visible-lg-block'></div>";
 	$count = 0;
 } ?>
