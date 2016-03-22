46933a5d1b04cf1bcd868b3bc3abd031c4a60edd
skotperez <voraginebramante@gmail.com>
2016-03-22 23:06:09 +0100

    Styles default page template

--- page.php
+++ page.php
@@ -14,8 +14,8 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary" class="row">
+		<main id="main" class="col-md-6" role="main">
 
 			<?php
 			while ( have_posts() ) : the_post();
@@ -34,5 +34,4 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
