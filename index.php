f12c9ce08cd2fc1a32edd4b497cd0fe03bd50085
skotperez <voraginebramante@gmail.com>
2016-03-23 14:15:51 +0100

    Modifies blog page template

--- index.php
+++ index.php
@@ -14,43 +14,40 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary" class="row">
+		<main id="main" class="col-md-6" role="main">
 
-		<?php
-		if ( have_posts() ) :
+		<?php if ( have_posts() ) :
+			$pt = "declarations";
+			global $wp_post_types;
+			if ( $wp_post_types[$pt]->description != '' )
+				$page_desc = "<header><div class='ur-3cols'>" .$wp_post_types[$pt]->description. "</div></header>";
+			else $page_desc = "";
+			//echo $page_desc;?>
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-
+			<div class="media-list">
 			<?php
-			endif;
-
 			/* Start the Loop */
+			$count = 0;
 			while ( have_posts() ) : the_post();
-
 				/*
 				 * Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+					get_template_part( 'template-parts/content', get_post_format() );
 
-			endwhile;
-
-			the_posts_navigation();
+					endwhile; ?>
+			</div>
+			<?php the_posts_navigation();
 
 		else :
 
-			get_template_part( 'template-parts/content', 'none' );
+//			get_template_part( 'template-parts/content', 'none' );
 
 		endif; ?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
