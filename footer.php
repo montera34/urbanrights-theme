de6b4aaf6b14f445e2b50647e47f215eb205deb5
Alfonso Sánchez Uzábal <skotperez@voragine.net>
2019-01-12 14:21:23 +0100

    Changes logos in header and footer and favicon

--- footer.php
+++ footer.php
@@ -23,16 +23,13 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<div class="navbar-text navbar-left"><?php _e('A project by','urbanrights'); ?>: <a href="http://zuloark.com">Zuloark</a><br /><?php _e('Curator','urbanrights'); ?>: Julia Förster</div>
+			<div class="navbar-text navbar-left"><?php _e('A project by','urbanrights'); ?></br><a href="http://zuloark.com">Zuloark</a></div>
 			<a class="navbar-brand" href="http://urbanrights.org"><img src="<?php echo URBANRIGHTS_BLOGTHEME; ?>/images/ur.png" alt="The Universal Declaration of Urban Rights" /></a>
 		</div>
 		<div class="collapse navbar-collapse" id="bottom-navbar-collapse">
 			<div class="navbar-text navbar-left"><?php _e('Cooperations partners','urbanrights'); ?>:</div>
 			<ul class="nav navbar-nav navbar-left">
-				<li><a href="http://www.adk.de"><img src="<?php echo URBANRIGHTS_BLOGTHEME; ?>/images/colabora-adk.jpg" alt="Academie der Künste" /></a></li>
-				<li><a href="http://berlin.cervantes.es"><img src="<?php echo URBANRIGHTS_BLOGTHEME; ?>/images/colabora-icervantes.jpg" alt="Instituto Cercantes" /></a></li>
-				<li><a href="http://www.accioncultural.es/"><img src="<?php echo URBANRIGHTS_BLOGTHEME; ?>/images/colabora-ace.jpg" alt="Acción Cultural Española" /></a></li>
-				<li><a href="http://www.beliusstiftung.org/"><img src="<?php echo URBANRIGHTS_BLOGTHEME; ?>/images/colabora-belius.jpg" alt="Belius Stiftung" /></a></li>
+				<li>Here cooperators logos.</li>
 			</ul>
 		</div>
 	</div>
--- header.php
+++ header.php
@@ -1,40 +1,40 @@
 <?php
-/**
- * The header for our theme.
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Urban_Rights
- */
+	/**
+	 * The header for our theme.
+	 *
+	 * This is the template that displays all of the <head> section and everything up until <div id="content">
+	 *
+	 * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+	 *
+	 * @package Urban_Rights
+	 */
 
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	?><!DOCTYPE html>
+	<html <?php language_attributes(); ?>>
+	<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<?php wp_head(); ?>
-</head>
+	<?php wp_head(); ?>
+	</head>
 
-<body <?php body_class(); ?>>
+	<body <?php body_class(); ?>>
 
-<header id="masthead" class="site-header" role="banner">
+	<header id="masthead" class="site-header" role="banner">
 
-	<nav id="top-navbar" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-	<div class="container">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#top-navbar-collapse">
-				<span class="sr-only">Show/hide menu</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' )); ?>"><img src="<?php echo URBANRIGHTS_BLOGTHEME; ?>/images/ur.berlin.png" alt="<?php echo URBANRIGHTS_BLOGNAME; ?>" /></a>
+		<nav id="top-navbar" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#top-navbar-collapse">
+					<span class="sr-only">Show/hide menu</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' )); ?>"><img src="<?php echo URBANRIGHTS_BLOGTHEME; ?>/images/ur.png" alt="<?php echo URBANRIGHTS_BLOGNAME; ?>" /></a>
 		</div>
 		<div class="collapse navbar-collapse" id="top-navbar-collapse">
 			<?php $location = "primary";
