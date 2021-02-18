ce615f569b14ff95f06c1f11fa57a716b15e5f13
Alfonso Sánchez Uzábal <skotperez@voragine.net>
2021-02-18 17:07:16 +0100

    feat: add website name in header

--- header.php
+++ header.php
@@ -1,27 +1,27 @@
 <?php
-	/**
-	 * The header for our theme.
-	 *
-	 * This is the template that displays all of the <head> section and everything up until <div id="content">
-	 *
-	 * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
-	 *
-	 * @package Urban_Rights
-	 */
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Urban_Rights
+ */
 
-	?><!DOCTYPE html>
-	<html <?php language_attributes(); ?>>
-	<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-	<?php wp_head(); ?>
-	</head>
+<?php wp_head(); ?>
+</head>
 
-	<body <?php body_class(); ?>>
+<body <?php body_class(); ?>>
 
 	<header id="masthead" class="site-header" role="banner">
 
@@ -35,8 +35,9 @@
 					<span class="icon-bar"></span>
 				</button>
 				<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' )); ?>"><img src="<?php echo URBANRIGHTS_BLOGTHEME; ?>/images/ur.png" alt="<?php echo URBANRIGHTS_BLOGNAME; ?>" /></a>
-		</div>
-		<div class="collapse navbar-collapse" id="top-navbar-collapse">
+				<p class="navbar-text site-tit"><?php echo URBANRIGHTS_BLOGNAME; ?></p>
+			</div>
+			<div class="collapse navbar-collapse" id="top-navbar-collapse">
 			<?php $location = "primary";
 			if ( has_nav_menu( $location ) ) {
 				$args = array(
@@ -58,10 +59,9 @@
 				wp_nav_menu( $args );
 			}
 			?>
+			</div>
 		</div>
-	</div>
 	</nav>
-	<h1 class='hide'></h1>
 </header><!-- #masthead -->
 
 <div id="content" <?php urbanrights_container_class(); ?>>
